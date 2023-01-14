b39e9e783e19be6ad0368c98fbc89c97b4216bd2
Galiya <safaga71@mail.ru>
2023-01-14 15:20:53 +0300

    Prog

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число
+//чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+Console.WriteLine("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+uint numberN2 = numberN%2 == 0;
+Console.WriteLine(numberN2);
+
