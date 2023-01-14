3d50e9e2d79002224c4d31ac368be7325c5e921a
Galiya <safaga71@mail.ru>
2023-01-14 17:09:08 +0300

    new file

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,8 +3,25 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
-Console.WriteLine("Введите число N: ");
+/*Console.WriteLine("Введите число N: ");
 int numberN = int.Parse(Console.ReadLine());
-uint numberN2 = numberN%2 == 0;
-Console.WriteLine(numberN2);
+int numberN2 = numberN%2 == 0;
+Console.WriteLine(numberN2);*/
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+int temp = A % 2;
+bool result = temp == 0;
+
+   if(result)
+{ 
+    Console.WriteLine("Да");
+}
+   else
+{
+    Console.WriteLine("Нет");
+}
+
+
+
 
